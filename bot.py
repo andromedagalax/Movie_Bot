b5ba92a2d4f5094c5f05703eb53a91c08400402d
andromedagalax <andromedagalaxynearas@gmail.com>
2024-05-08 12:16:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,4 +99,5 @@ if __name__ == '__main__':
         loop.run_until_complete(start())
     except KeyboardInterrupt:
         logging.info('Service Stopped Bye 👋')
-
+    except OverflowError as e:
+        logging.error(f"OverflowError: {e}. Please check the value assigned to self.api_id.")
