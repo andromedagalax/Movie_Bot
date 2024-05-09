0c73b0c40c47b2df475baaa9db6b700b9198ebb8
andromedagalax <andromedagalaxynearas@gmail.com>
2024-05-09 11:22:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'TechVJBot')
-API_ID = float(environ.get('API_ID', ''))
+API_ID = int(environ['API_ID'])
 API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
