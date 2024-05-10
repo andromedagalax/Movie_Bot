4951c4ef7b26788830dea6b6fee5c7692190acf0
andromedagalax <andromedagalaxynearas@gmail.com>
2024-05-10 18:06:05 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 from pyrogram import Client, filters
 from info import CHANNELS
 from database.ia_filterdb import save_file
