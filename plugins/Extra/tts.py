98b80d0e2319395b215ae11d82b554992f516594
andromedagalax <andromedagalaxynearas@gmail.com>
2024-05-10 17:43:47 +0530

    Update tts.py

--- plugins/Extra/tts.py
+++ plugins/Extra/tts.py
@@ -1,8 +1,3 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-
 import traceback
 from asyncio import get_running_loop
 from io import BytesIO
