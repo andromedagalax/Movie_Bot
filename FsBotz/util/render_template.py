baba1d0949efe2892273ae855cb3b1a133ce12a9
andromedagalax <andromedagalaxynearas@gmail.com>
2024-05-10 17:41:44 +0530

    Update render_template.py

--- FsBotz/util/render_template.py
+++ FsBotz/util/render_template.py
@@ -25,9 +25,9 @@ async def render_page(id, secure_hash, src=None):
     tag = file_data.mime_type.split("/")[0].strip()
     file_size = humanbytes(file_data.file_size)
     if tag in ["video", "audio"]:
-        template_file = "TechVJ/template/req.html"
+        template_file = "FsBotz/template/req.html"
     else:
-        template_file = "TechVJ/template/dl.html"
+        template_file = "FsBotz/template/dl.html"
         async with aiohttp.ClientSession() as s:
             async with s.get(src) as u:
                 file_size = humanbytes(int(u.headers.get("Content-Length")))
