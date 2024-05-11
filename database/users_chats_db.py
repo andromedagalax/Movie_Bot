12196ce4a686241c2cf4025ca814378c633054b7
andromedagalax <andromedagalaxynearas@gmail.com>
2024-05-11 09:47:14 +0530

    Update users_chats_db.py

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -41,7 +41,7 @@ class Database:
         self.db = self._client[database_name]
         self.col = self.db.users
         self.grp = self.db.groups
-        self.users = self.db.uersz
+        self.users = self.db.users
 
 
     def new_user(self, id, name):
