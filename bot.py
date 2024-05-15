4272addb32450d6f7f4288988251a4d7c9c0360d
FsBotz 1 <andromedagalaxynearas@gmail.com>
2024-05-15 11:35:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -66,7 +66,7 @@ async def start():
             load = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(load)
             sys.modules["plugins." + plugin_name] = load
-            print("Tech VJ Imported => " + plugin_name)
+            print("FsBotz Imported => " + plugin_name)
     if ON_HEROKU:
         asyncio.create_task(ping_server())
     b_users, b_chats = await db.get_banned()
