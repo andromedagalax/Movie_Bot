9049bcf420a3b539bbf04626b93b211baf4fad4c
FsBotz 1 <andromedagalaxynearas@gmail.com>
2024-05-15 12:22:38 +0530

    Update repo.py

--- plugins/Extra/repo.py
+++ plugins/Extra/repo.py
@@ -30,7 +30,7 @@ async def git(bot, message):
 
 <b>Open Issues :</b> <i>{qw.get("open_issues")}</i>
 
-<b>Powered by :</b> @VJ_Botz
+<b>Powered by :</b> @Fs_Botz
 
 """
         if qw.get("description"):
