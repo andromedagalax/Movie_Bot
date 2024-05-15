0d135fafcaba4aef71a29f06ce1e7aa05b243fa6
FsBotz 1 <andromedagalaxynearas@gmail.com>
2024-05-15 12:24:07 +0530

    Update share_text.py

--- plugins/Extra/share_text.py
+++ plugins/Extra/share_text.py
@@ -11,7 +11,7 @@ async def share_text(client, message):
     else:
         await vj.reply_text(
             text=f"**Notice:**\n\n1. Send Any Text Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",               
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Updates Channel", url=f"https://t.me/vj_botz")]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Updates Channel", url=f"https://t.me/Fs_Botz")]])
             )                                                   
         return
     await vj.reply_text(
