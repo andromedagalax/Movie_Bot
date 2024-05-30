df571de4b175d9a5ac657c94ef7f176babc08abe
FsBotz 1 <andromedagalaxynearas@gmail.com>
2024-05-30 21:13:51 +0530

    Update button.py

--- plugins/Extra/button.py
+++ plugins/Extra/button.py
@@ -7,14 +7,14 @@ from FsBotz.bot import FsBotz
 keyboard1 = ReplyKeyboardMarkup(
     resize_keyboard=True,
     one_time_keyboard=True,
-    keyboard=[[" Hello!", "Channel"]]  # List containing button labels
+    keyboard=[["👋 Hello!", "Channel"]]  # List containing button labels
 )
 
 @FsBotz.message_handler()
 async def kb_answer(message: types.Message):
-    if message.text == '👋 Hello!':
+    if message.text == "👋 Hello!":
         await message.reply("Hi! How are you?")
-    elif message.text == 'Channel':
+    elif message.text == "Channel":
         await message.reply("https://t.me/fs_moviez_channel")
     else:
         await message.reply(f"Your message is: {message.text}")
