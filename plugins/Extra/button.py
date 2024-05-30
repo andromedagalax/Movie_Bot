f9ef00ef5f6bc8ae715fde1dc348da96d8ef2cbe
FsBotz 1 <andromedagalaxynearas@gmail.com>
2024-05-30 20:06:33 +0530

    Update Extra

---
+++ plugins/Extra/button.py
@@ -0,0 +1,16 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from random import randint
+from FsBotz.bot import FsBotz
+
+keyboard1 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add("👋 Hello!", "Channel")
+
+@FsBotz.message_handler()
+async def kb_answer(message: types.Message):
+    if message.text == '👋 Hello!':
+        await message.reply("Hi! How are you?")
+    elif message.text == 'Channel':
+        await message.reply("https://t.me/fs_moviez_channel")
+    else:
+        await message.reply(f"Your message is: {message.text}")
