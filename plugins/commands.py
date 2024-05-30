2bc7f3449b7c1885a7d81c325c08beddd4bfe919
FsBotz 1 <andromedagalaxynearas@gmail.com>
2024-05-30 21:20:03 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -15,7 +15,7 @@ from database.users_chats_db import db, delete_all_referal_users, get_referal_us
 from info import CHANNELS, ADMINS, SHORTLINK_MODE, PREMIUM_AND_REFERAL_MODE, STREAM_MODE, AUTH_CHANNEL, OWNER_USERNAME, REFERAL_PREMEIUM_TIME, REFERAL_COUNT, PAYMENT_TEXT, PAYMENT_QR, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, CHNL_LNK, GRP_LNK, REQST_CHANNEL, SUPPORT_CHAT_ID, SUPPORT_CHAT, MAX_B_TN, VERIFY, SHORTLINK_API, SHORTLINK_URL, TUTORIAL, VERIFY_TUTORIAL, IS_TUTORIAL, URL
 from utils import get_settings, pub_is_subscribed, get_size, is_subscribed, save_group_settings, temp, verify_user, check_token, check_verification, get_token, get_shortlink, get_tutorial, get_seconds
 from database.connections_mdb import active_connection
-from plugins.Extra.button import keyboard1
+
 # from plugins.pm_filter import ENABLE_SHORTLINK
 import re, asyncio, os, sys
 import json
@@ -23,6 +23,9 @@ import base64
 from urllib.parse import quote_plus
 from FsBotz.util.file_properties import get_name, get_hash, get_media_file_size
 logger = logging.getLogger(__name__)
+
+keyboard1 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add("👋 Hello!", "Channel")
+
 
 BATCH_FILES = {}
 
