dbf71128c66b93ebd1cbcf28d634ba7edf6a7722
FsBotz 1 <andromedagalaxynearas@gmail.com>
2024-07-04 08:17:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -108,7 +108,7 @@ EPISODES = ["E01", "E02", "E03", "E04", "E05", "E06", "E07", "E08", "E09", "E10"
 QUALITIES = ["360p", "480p", "720p", "1080p", "1440p", "2160p"]
 
 # Online Stream and Download
-STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or Flase
+STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or Flase
 
 # If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 MULTI_CLIENT = False
