26ead49e3112de5a4650f722fc1f9a0683381ddb
FsBotz 1 <andromedagalaxynearas@gmail.com>
2024-07-26 19:32:26 +0530

    Update yt_dl.py

--- plugins/Extra/yt_dl.py
+++ plugins/Extra/yt_dl.py
@@ -43,7 +43,7 @@ async def song(client, message):
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
 
-        cap = "**BY›› [VJ NETWORKS™](https://t.me/vj_bots)**"
+        cap = "**BY›› [FSBOTZ™](https://t.me/Fs_Botz)**"
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
