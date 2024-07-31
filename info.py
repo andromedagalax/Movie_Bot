5e8190eb12d18074b94934bbacf83ca666d215f6
FsBotz 1 <andromedagalaxynearas@gmail.com>
2024-07-31 18:34:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -119,7 +119,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://movie-bot-by-fsbotz.onrender.com/")
+URL = environ.get("URL", "https://fs-botz.onrender.com/")
 
 
 # Rename Info ; If True Then Bot Rename File Else Not
