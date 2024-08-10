583d93302504d7c486291ee7c6c1b23a76f30245
FsBotz 1 <andromedagalaxynearas@gmail.com>
2024-08-10 11:43:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,6 +24,7 @@ from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
 from info import *
+from info import ADMINS
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
